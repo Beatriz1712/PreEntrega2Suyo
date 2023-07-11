b31b54903f2edfe687b9754546d268f02d9237a4
Beatriz <beatriz1712sc@gmail.com>
2023-07-10 19:37:50 -0500

    creando data productos.json

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,19 +1,54 @@
-import ItemCount from "../ItemCount/ItemCount"
+//import ItemCount from "../ItemCount/ItemCount"
+import data from"../data/productos.json"
+import { useState,useEffect } from "react"
+
+const ItemListContainer = () => {
+ 
+        const [productos,setProductos] = useState([]);
+        console.log(productos);
+
+        const pedirProductos = () => {
+          return new Promise((resolve, reject) => {
+            resolve(data)
+          })
+        } 
+      useEffect(() => {
+          pedirProductos()
+           .then((res) => {
+                setProductos (res);
+           })
+      }, [])
+
+       
+        return (
+          <div>
+            {
+              productos.length > 0 &&
+              <div>
+                <img src = {productos[0].image} alt= {productos[0].titulo}></img>
+                 <h2>{productos[0].titulo}</h2> 
+                 <p>{productos[0].precio}</p>
+                 <p>{productos[0].description}</p>
+              </div>
+            }       
+          </div>
+        )
 
 
-const ItemListContainer = ({greeting}) => {
 
+
+   
 
 
 
     
-  return (
+  /*return (
     <div>
         <h1 style ={{fontSize:"2.5rem",color:"black",margin:"20px"}}>{greeting}</h1>
         
         <ItemCount stock={10} initial={1} onAdd={(quantity) => console.log("Cantidad agregada")} />
     </div>
-  )
-}
+  )*/
 
-export default ItemListContainer
+};
+export default ItemListContainer;
